8f7a4d76c8035978f970e8c5e898e254dd86490d
BelChaparro <a.belenchaparro@gmail.com>
2022-04-20 05:00:31 -0300

    created index.js file and included last update date to html element in footer

---
+++ js/index.js
@@ -0,0 +1,3 @@
+// Assigns the last modification date to the element with an ID of "last-update-date":
+const lastModifiedDate =  document.lastModified;
+document.querySelector("#last-update-date").textContent = lastModifiedDate;
