536183fb5d5f19e57967726b19b15c09f6adccd2
BelChaparro <a.belenchaparro@gmail.com>
2022-04-20 22:05:34 -0300

    dynamically friven current year for footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// Get and assign current year to HTML element with an ID of "current-year":
+const currentYear = new Date().getFullYear();
+document.querySelector("#current-year").textContent = currentYear;
+
 // Assigns the last modification date to the element with an ID of "last-update-date":
 const lastModifiedDate =  document.lastModified;
 document.querySelector("#last-update-date").textContent = lastModifiedDate;
