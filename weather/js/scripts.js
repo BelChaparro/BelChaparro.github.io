ebcab0a40aadf9da719a4ad5f0ace011cf6a293d
BelChaparro <a.belenchaparro@gmail.com>
2022-05-04 18:52:36 -0300

    updated date format to match assignment

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -9,5 +9,13 @@ x.onclick = toggleMenu;
 
 //Get current date:
 // format: Wednesday, 24 July 2020
-let currentDate = new Date().toLocaleDateString("en", {weekday: "long", day: "numeric", month: "long", year: "numeric"});
+let currentDate = new Date();
+
+let weekday = currentDate.toLocaleDateString("en", {weekday: "long"});
+let day = currentDate.getDate();
+let month = currentDate.toLocaleDateString("en", {month: "long",});
+let year = currentDate.getFullYear();
+
+currentDate = `${weekday}, ${day} ${month} ${year}`; 
+
 document.querySelector("#current-date").textContent = currentDate;
