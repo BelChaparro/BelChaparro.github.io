6b5fb2ecd3851b987bcbedd6efb66d510cded6c6
BelChaparro <a.belenchaparro@gmail.com>
2022-05-06 01:00:36 -0300

    added bannerAd function to show banner on Fridays

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -7,6 +7,7 @@ function toggleMenu() {
 const x = document.querySelector("#menu-btn");
 x.onclick = toggleMenu;
 
+
 //Get current date:
 // format: Wednesday, 24 July 2020
 let currentDate = new Date();
@@ -16,6 +17,15 @@ let day = currentDate.getDate();
 let month = currentDate.toLocaleDateString("en", {month: "long",});
 let year = currentDate.getFullYear();
 
-currentDate = `${weekday}, ${day} ${month} ${year}`; 
-
+currentDate = `${weekday}, ${day} ${month} ${year}`;
 document.querySelector("#current-date").textContent = currentDate;
+
+
+// Banner Ad:
+function bannerAd() {
+    document.querySelector(".banner-ad").style.display = "block";
+}
+console.log(weekday.toLowerCase());
+if (weekday.toLowerCase() == "friday") {
+    bannerAd;
+}
