763502c46279ea59767cfea19bc4562c5c5b7525
BelChaparro <a.belenchaparro@gmail.com>
2022-05-20 01:34:42 -0300

    changed variables names

--- weather/js/banner.js
+++ weather/js/banner.js
@@ -1,11 +1,11 @@
 // Banner Ad:
 let date = new Date();
-let weekday = date.toLocaleDateString("en", {weekday: "long"});
+let dayOfWeek = date.toLocaleDateString("en", {weekday: "long"});
 
 function bannerAd() {
     document.querySelector(".banner-ad").style.display = "block";
 }
 
-if (weekday == "Friday") {
+if (dayOfWeek == "Friday") {
     bannerAd();
 }
